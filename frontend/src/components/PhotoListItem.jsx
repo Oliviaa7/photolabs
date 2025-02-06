d8d08041779d34c0fc55313693979fa5492e21e8
Oliviaa7 <olivialanderson1994@gmail.com>
2025-02-05 20:49:02 -0800

    modified classNames to incorporate given styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,13 +7,17 @@ const PhotoListItem = (props) => {
   return (
     <article className="photo-list__item">
       <div key={photoData.id}>
-        <img src={photoData.imageSource} alt="Post" className="photo-list__image"/>
-        <img src={photoData.profile} alt="Profile" className="photo-list__user-profile"/>
-        <h3>{photoData.username}</h3>
-        <h4 className="photo-list__user-location">{photoData.location.city}, {photoData.location.country}</h4>
+        <img src={photoData.imageSource} alt="Post" className="photo-list__image" />
+        <div className="photo-list__user-details">
+          <img src={photoData.profile} alt="Profile" className="photo-list__user-profile" />
+          <div className="photo-list__user-info">
+            <h3>{photoData.username}</h3>
+            <h4 className="photo-list__user-location">{photoData.location.city}, {photoData.location.country}</h4>
+          </div>
+        </div>
       </div>
     </article>
-  )
+  );
 };
 
 export default PhotoListItem;
