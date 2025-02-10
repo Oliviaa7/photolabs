37a15db9512b34a526421e8533c95a2c32017684
Oliviaa7 <olivialanderson1994@gmail.com>
2025-02-09 16:48:55 -0800

    refactored to use PhotoList instead of copy-pasted data inApp.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,14 @@
 import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
+import PhotoList from 'components/PhotoList';
 import './App.scss';
-import PhotoFavButton from 'components/PhotoFavButton';
 
 // Note: Rendering a single component to build components in isolation
-
-const photos = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "2",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "3",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-
-]
 
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem photoData={photos}/>
+      <PhotoList />
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,12 +5,7 @@ import PhotoFavButton from "./PhotoFavButton";
 const PhotoListItem = (props) => {
   const { photoData } = props;
 
-  const [selectedPhotos, setSelectedPhotos] = useState(
-    photoData.reduce((acc, photo) => {
-      acc[photo.id] = false;
-      return acc;
-    }, {})
-  );
+  const [selectedPhotos, setSelectedPhotos] = useState({});
 
   const handleClick = (id) => {
     setSelectedPhotos((prevSelectedPhotos) => ({
@@ -19,30 +14,22 @@ const PhotoListItem = (props) => {
     }));
   };
 
-  const photoDataHandler = photoData.map((data) => {
-
-    return (
-      <article key={data.id} className="photo-list__item">
-        <img src={data.imageSource} alt="Post" className="photo-list__image" />
-
-        <PhotoFavButton onClick={() => handleClick(data.id)} selected={selectedPhotos[data.id]}/>
-
-        <div className="photo-list__user-details">
-          <img src={data.profile} alt="Profile" className="photo-list__user-profile" />
-          <div className="photo-list__user-info">
-            <h3>{data.username}</h3>
-            <h4 className="photo-list__user-location">{data.location.city}, {data.location.country}</h4>
-          </div>
-        </div>
-      </article>
-    );
-  });
-
   return (
-    <section className="photo-list">
-      {photoDataHandler}
-    </section>
+    <article key={photoData.id} className="photo-list__item">
+      <img src={photoData.urls.regular} alt="Post" className="photo-list__image" />
+
+      <PhotoFavButton onClick={() => handleClick(photoData.id)} selected={selectedPhotos[photoData.id]} />
+
+      <div className="photo-list__user-details">
+        <img src={photoData.user.profile} alt="Profile" className="photo-list__user-profile" />
+        <div className="photo-list__user-info">
+          <h3>{photoData.user.username}</h3>
+          <h4 className="photo-list__user-location">{photoData.location.city}, {photoData.location.country}</h4>
+        </div>
+      </div>
+    </article>
   );
+
 };
 
 export default PhotoListItem;
