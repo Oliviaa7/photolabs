29880dffa7c1f6180f102b08d3311d62fa7315cf
Oliviaa7 <olivialanderson1994@gmail.com>
2025-02-10 21:18:05 -0800

    formatted the top bar, did not add functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import PhotoListItem from './components/PhotoListItem';
 import PhotoList from 'components/PhotoList';
+import TopNavigationBar from 'components/TopNavigationBar';
 import './App.scss';
-import TopicList from 'components/TopicList';
 
 // Note: Rendering a single component to build components in isolation
 
 const App = () => {
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigationBar />
       <PhotoList />
     </div>
   );
