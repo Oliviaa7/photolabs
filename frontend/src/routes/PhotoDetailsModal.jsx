cd728436c71e0c79dd8410c7a7b07526eecaac3d
Oliviaa7 <olivialanderson1994@gmail.com>
2025-02-11 18:54:22 -0800

    added close functionality to modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -6,7 +6,7 @@ import closeSymbol from '../assets/closeSymbol.svg';
 const PhotoDetailsModal = ({ photo, closeModal }) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={closeModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
